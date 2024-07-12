d33f7aaa5ba96d88af2731fd43128044c739ea06
abilllll10 <abilaisy2003@gmail.com>
2024-07-12 17:08:01 +0700

    baruu

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,22 @@
-import "./login.scss"; 
-import { useContext,useState } from "react";
+import "./login.scss";
+import { useContext, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
 
-const Login = () => { 
+const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { dispatch: dispatchMode, darkMode } = useContext(DarkModeContext);
 
-  const navitage = useNavigate()
+  const navitage = useNavigate();
 
-  const {dispatch} = useContext(AuthContext)
+  const { dispatch: dispatchAuth } = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -20,10 +24,9 @@ const Login = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user; 
-        dispatch({type:"LOGIN", payload:user});
+        const user = userCredential.user;
+        dispatchAuth({ type: "LOGIN", payload: user });
         navitage("/");
-        console.log(user);
       })
       .catch((error) => {
         setError(true);
@@ -32,18 +35,13 @@ const Login = () => {
 
   return (
     <div className="login">
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="email" 
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password" 
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
+      <form data-testid="form" onSubmit={handleLogin}>
+        <input id="email" type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
+        <input id="password" type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit" data-testid="submit">
+          Login
+        </button>
+        <div className="item">{darkMode ? <LightModeOutlinedIcon className="icon" onClick={() => dispatchMode({ type: "TOGGLE" })} /> : <DarkModeOutlinedIcon className="icon" onClick={() => dispatchMode({ type: "TOGGLE" })} />}</div>
         {error && <span>Wrong email or password!</span>}
       </form>
     </div>
